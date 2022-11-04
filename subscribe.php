aa9bf64b004fdad0dd042c328da5173a1f398cae
chenxi-a11y <114125366+chenxi-a11y@users.noreply.github.com>
2022-11-04 11:38:57 +0800

    Add files via upload

--- subscribe.php
+++ subscribe.php
@@ -85,6 +85,33 @@
 }"},
 {"id":5005,
 "type":"amy",
+"name":"\u767e\u5ea6\u76f4\u8fde-\u53ef\u6e38\u620f-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
+"model":"{
+  \"Socks5ListenAddr\": \":10800\",
+  \"PidFile\": \"amy4.pid\",
+  \"DnsListenAddr\": \":10530\",
+  \"\/\/\": \"\u4e0d\u8981\u778eJB\u4e71\u4fee\u6539\",
+  \"AmyOuts\": [{
+    \"Tag\": \"ANet\",
+    \"ProxyType\": \"ANet\",
+    \"ANetSettings\": {
+      \"DestAddrs\": [\"120.48.21.141:80\"],
+      \"DestAddrsStrategy\": \"IP\",
+      \"ServerKey\": \"ml.wezyo.com\",
+      \"FFHost\": \"cloudnproxy.baidu.com\",
+      \"HTTPRequest\": \"GET \/ HTTP\/1.1\\r\\nHost: cloudnproxy.baidu.com\\r\\n\\r\\n\",
+            \"TCPPool\": {
+        \"MaxConn\": 60,
+        \"MinConn\": 5,
+        \"DialSpeed\": 15,
+        \"IdleSec\": 115
+      }
+    }
+  }],
+  \"Routers\": []
+}"},
+{"id":5006,
+"type":"amy",
 "name":"\u817e\u8baf\u738b\u5361\u300140G\u5185",
 "model":"{
   \"Socks5ListenAddr\": \":10800\",
@@ -110,7 +137,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5006,
+{"id":5007,
 "type":"amy",
 "name":"\u817e\u8baf\u738b\u53612\u300140G\u5185",
 "model":"{
@@ -137,7 +164,34 @@
   }],
   \"Routers\": []
 }"},
-{"id":5007,
+{"id":5008,
+"type":"amy",
+"name":"\u5927\u738b\u5361-\u53ef\u6e38\u620f-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
+"model":"{
+  \"Socks5ListenAddr\": \":10800\",
+  \"PidFile\": \"amy4.pid\",
+  \"DnsListenAddr\": \":10530\",
+  \"\/\/\": \"\u4e0d\u8981\u778eJB\u4e71\u4fee\u6539\",
+  \"AmyOuts\": [{
+    \"Tag\": \"ANet\",
+    \"ProxyType\": \"ANet\",
+    \"ANetSettings\": {
+      \"DestAddrs\": [\"120.48.21.141:80\"],
+      \"DestAddrsStrategy\": \"IP\",
+      \"ServerKey\": \"ml.wezyo.com\",
+      \"FFHost\": \"game.gtimg.cn\",
+      \"HTTPRequest\": \"GET \/ HTTP\/1.1\\r\\nHost: game.gtimg.cn\\r\\n\\r\\n\",
+            \"TCPPool\": {
+        \"MaxConn\": 60,
+        \"MinConn\": 5,
+        \"DialSpeed\": 15,
+        \"IdleSec\": 115
+      }
+    }
+  }],
+  \"Routers\": []
+}"},
+{"id":5009,
 "type":"amy",
 "name":"\u9489\u94891 \u9002\u7528\uff1a\u963f\u91cc\u5b9d\u5361\u3001\u6c83\u6d3e0\u5143\u963f\u91cc\u6743\u76ca\u5305\u3001\u51b0\u9489\u9489\u5361\u7b49\u963f\u91cc\u7cfb",
 "model":"{
@@ -164,7 +218,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5008,
+{"id":5010,
 "type":"amy",
 "name":"\u9489\u94892",
 "model":"{
@@ -191,7 +245,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5009,
+{"id":5011,
 "type":"amy",
 "name":"\u9489\u94893",
 "model":"{
@@ -218,7 +272,34 @@
   }],
   \"Routers\": []
 }"},
-{"id":5010,
+{"id":5012,
+"type":"amy",
+"name":"\u9489\u9489-\u53ef\u6e38\u620f-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
+"model":"{
+  \"Socks5ListenAddr\": \":10800\",
+  \"PidFile\": \"amy4.pid\",
+  \"DnsListenAddr\": \":10530\",
+  \"\/\/\": \"\u4e0d\u8981\u778eJB\u4e71\u4fee\u6539\",
+  \"AmyOuts\": [{
+    \"Tag\": \"ANet\",
+    \"ProxyType\": \"ANet\",
+    \"ANetSettings\": {
+      \"DestAddrs\": [\"120.48.21.141:80\"],
+      \"DestAddrsStrategy\": \"IP\",
+      \"ServerKey\": \"ml.wezyo.com\",
+      \"FFHost\": \"tms.dingtalk.com\",
+      \"HTTPRequest\": \"GET \/ HTTP\/1.1\\r\\nHost: tms.dingtalk.com\\r\\n\\r\\n\",
+            \"TCPPool\": {
+        \"MaxConn\": 60,
+        \"MinConn\": 5,
+        \"DialSpeed\": 15,
+        \"IdleSec\": 115
+      }
+    }
+  }],
+  \"Routers\": []
+}"},
+{"id":5013,
 "type":"amy",
 "name":"\u5408\u4f5cA \u9002\u7528\uff1a\u6296\u97f3/\u5934\u6761\u5b9a\u5411\u5305 \u5218\u90a6\u5361\u3001\u5bfc\u5b66\u6613\u5b66\u5361\u3001\u7535\u4fe1\u5927\u9ed1\u725b\u3001\u5c0f\u6b6a\u5361\u7b49",
 "model":"{
@@ -245,8 +326,35 @@
   }],
   \"Routers\": []
 }"},
+{"id":5014,
+"type":"amy",
+"name":"\u5408\u4f5cA-\u53ef\u6e38\u620f-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
+"model":"{
+  \"Socks5ListenAddr\": \":10800\",
+  \"PidFile\": \"amy4.pid\",
+  \"DnsListenAddr\": \":10530\",
+  \"\/\/\": \"\u4e0d\u8981\u778eJB\u4e71\u4fee\u6539\",
+  \"AmyOuts\": [{
+    \"Tag\": \"ANet\",
+    \"ProxyType\": \"ANet\",
+    \"ANetSettings\": {
+      \"DestAddrs\": [\"120.48.21.141:80\"],
+      \"DestAddrsStrategy\": \"IP\",
+      \"ServerKey\": \"ml.wezyo.com\",
+      \"FFHost\": \"dm.toutiao.com\",
+      \"HTTPRequest\": \"GET \/ HTTP\/1.1\\r\\nHost: dm.toutiao.com\\r\\n\\r\\n\",
+            \"TCPPool\": {
+        \"MaxConn\": 60,
+        \"MinConn\": 5,
+        \"DialSpeed\": 15,
+        \"IdleSec\": 115
+      }
+    }
+  }],
+  \"Routers\": []
+}"},
 {
-"id":5011,
+"id":5015,
 "type":"amy",
 "name":"\u7535\u4fe1\u505c\u673a",
 "model":"{
@@ -290,7 +398,7 @@
     }
   }]
 }"},
-{"id":5012,
+{"id":5016,
 "type":"amy",
 "name":"\u7535\u4fe1\u505c\u673a2",
 "model":"{
@@ -317,7 +425,34 @@
   }],
   \"Routers\": []
 }"},
-{"id":5013,
+{"id":5017,
+"type":"amy",
+"name":"\u505c\u673a-\u53ef\u6e38\u620f-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
+"model":"{
+  \"Socks5ListenAddr\": \":10800\",
+  \"PidFile\": \"amy4.pid\",
+  \"DnsListenAddr\": \":10530\",
+  \"\/\/\": \"\u4e0d\u8981\u778eJB\u4e71\u4fee\u6539\",
+  \"AmyOuts\": [{
+    \"Tag\": \"ANet\",
+    \"ProxyType\": \"ANet\",
+    \"ANetSettings\": {
+      \"DestAddrs\": [\"120.48.21.141:80\"],
+      \"DestAddrsStrategy\": \"IP\",
+      \"ServerKey\": \"ml.wezyo.com\",
+      \"FFHost\": \"a.189.cn\",
+      \"HTTPRequest\": \"GET \/ HTTP\/1.1\\r\\nHost: a.189.cn\\r\\n\\r\\n\",
+            \"TCPPool\": {
+        \"MaxConn\": 60,
+        \"MinConn\": 5,
+        \"DialSpeed\": 15,
+        \"IdleSec\": 115
+      }
+    }
+  }],
+  \"Routers\": []
+}"},
+{"id":5018,
 "type":"amy",
 "name":"\u5168\u56fd\u8054\u901a\u624b\u5385\u3001\u5e7f\u897f\u3001\u91cd\u5e86 \u53ef\u514d\uff0c\u5177\u4f53\u81ea\u6d4b",
 "model":"{
@@ -344,7 +479,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5014,
+{"id":5019,
 "type":"amy",
 "name":"\u5168\u56fd\u8054\u901a\u516c\u514d\uff0cWAP\u63a5\u5165\u70b9\u3001\u6ce8\u610f\u8981\u7528WAP\u63a5\u5165\u70b9 \u770b\u6e05\u695a\uff0c\u5177\u4f53\u81ea\u6d4b",
 "model":"{
@@ -371,7 +506,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5015,
+{"id":5020,
 "type":"amy",
 "name":"\u54d4\u54e9\u54d4\u54e9 \u9002\u7528\uff1a22\u5361\u300133\u5361\u3001B\u7ad9\u5b9a\u5411\u7b49",
 "model":"{
@@ -398,7 +533,34 @@
   }],
   \"Routers\": []
 }"},
-{"id":5016,
+{"id":5021,
+"type":"amy",
+"name":"\u54d4\u54e9\u54d4\u54e9-\u53ef\u6e38\u620f-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
+"model":"{
+  \"Socks5ListenAddr\": \":10800\",
+  \"PidFile\": \"amy4.pid\",
+  \"DnsListenAddr\": \":10530\",
+  \"\/\/\": \"\u4e0d\u8981\u778eJB\u4e71\u4fee\u6539\",
+  \"AmyOuts\": [{
+    \"Tag\": \"ANet\",
+    \"ProxyType\": \"ANet\",
+    \"ANetSettings\": {
+      \"DestAddrs\": [\"120.48.21.141:80\"],
+      \"DestAddrsStrategy\": \"IP\",
+      \"ServerKey\": \"ml.wezyo.com\",
+      \"FFHost\": \"i0.hdslb.com\",
+      \"HTTPRequest\": \"GET \/ HTTP\/1.1\\r\\nHost: i0.hdslb.com\\r\\n\\r\\n\",
+            \"TCPPool\": {
+        \"MaxConn\": 60,
+        \"MinConn\": 5,
+        \"DialSpeed\": 15,
+        \"IdleSec\": 115
+      }
+    }
+  }],
+  \"Routers\": []
+}"},
+{"id":5022,
 "type":"amy",
 "name":"\u5929\u7ffc\u4e91\u76d81 \u9002\u7528\uff1a\u7535\u4fe1\u5929\u7ffc\u4e91\u76d8\u5b9a\u5411\u6d41\u91cf\u5305",
 "model":"{
@@ -436,7 +598,7 @@
     }
   }]
 }"},
-{"id":5017,
+{"id":5023,
 "type":"amy",
 "name":"\u5929\u7ffc\u4e91\u76d82",
 "model":"{
@@ -449,6 +611,33 @@
     \"ProxyType\": \"ANet\",
     \"ANetSettings\": {
       \"DestAddrs\": [\"193.38.50.151:80\"],
+      \"DestAddrsStrategy\": \"IP\",
+      \"ServerKey\": \"ml.wezyo.com\",
+      \"FFHost\": \"download.cloud.189.cn\",
+      \"HTTPRequest\": \"GET \/ HTTP\/1.1\\r\\nHost: download.cloud.189.cn\\r\\n\\r\\n\",
+            \"TCPPool\": {
+        \"MaxConn\": 60,
+        \"MinConn\": 5,
+        \"DialSpeed\": 15,
+        \"IdleSec\": 115
+      }
+    }
+  }],
+  \"Routers\": []
+}"},
+{"id":5024,
+"type":"amy",
+"name":"\u5929\u7ffc\u4e91\u76d8-\u53ef\u6e38\u620f-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
+"model":"{
+  \"Socks5ListenAddr\": \":10800\",
+  \"PidFile\": \"amy4.pid\",
+  \"DnsListenAddr\": \":10530\",
+  \"\/\/\": \"\u4e0d\u8981\u778eJB\u4e71\u4fee\u6539\",
+  \"AmyOuts\": [{
+    \"Tag\": \"ANet\",
+    \"ProxyType\": \"ANet\",
+    \"ANetSettings\": {
+      \"DestAddrs\": [\"120.48.21.141:80\"],
       \"DestAddrsStrategy\": \"IP\",
       \"ServerKey\": \"ml.wezyo.com\",
       \"FFHost\": \"download.cloud.189.cn\",
