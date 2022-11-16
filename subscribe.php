a642168838a1c7870775d77a13c461b0e83cfc7f
chenxi-a11y <114125366+chenxi-a11y@users.noreply.github.com>
2022-11-17 00:30:25 +0800

    Add files via upload

--- subscribe.php
+++ subscribe.php
@@ -2,13 +2,18 @@
 {
 "id":5000,
 "type":"amy",
-"name":"\u4e0d\u5b9a\u65f6\u53d1\u5e03\u65b0\u6a21\u5f0f\uff0c\u5f53\u524d\u4e0a\u4f20\u65f6\u95f4\uff1a2022.11.7\uff0c\u8bb0\u5f97\u6bcf\u5929\u81f3\u5c11\u66f4\u65b0\u4e00\u6b21\u6a21\u5f0f",
+"name":"\u4e0d\u5b9a\u65f6\u53d1\u5e03\u65b0\u6a21\u5f0f\uff0c\u5f53\u524d\u4e0a\u4f20\u65f6\u95f4\uff1a2022.11.16\uff0c\u8bb0\u5f97\u6bcf\u5929\u81f3\u5c11\u66f4\u65b0\u4e00\u6b21\u6a21\u5f0f",
 "model":"\u6bcf\u5929\u4e0d\u5b9a\u65f6\u66f4\u65b0\u6a21\u5f0f\uff0c\u8bf7\u70b9\u4e0a\u65b9\u4e0b\u8f7d\u56fe\u6807\u66f4\u65b0\u6a21\u5f0f\u554a"},
 {"id":5001,
 "type":"amy",
 "name":"\u6a21\u5f0f\u6ca1\u7f51\u53ef\u80fd\u662f\u5931\u6548\u4e86,\u7b49\u8fc7\u534a\u5c0f\u65f6\u5de6\u53f3\u518d\u66f4\u65b0\u6a21\u5f0f\u5c31\u884c\u4e86,\u70b9\u4e0b\u8f7d\u56fe\u6807\u66f4\u65b0",
 "model":"\u6bcf\u5929\u4e0d\u5b9a\u65f6\u66f4\u65b0\u6a21\u5f0f\uff0c\u8bf7\u70b9\u4e0a\u65b9\u4e0b\u8f7d\u56fe\u6807\u66f4\u65b0\u6a21\u5f0f\u554a"},
-{"id":5002,
+{
+"id":5002,
+"type":"amy",
+"name":"\u5982\u679c\u611f\u89c9\u6162\u53ef\u4ee5\u4f7f\u7528\u6211\u4eec\u7684\u9ad8\u901f\u514d\u6d41:ml.wezyo.com",
+"model":"\u6bcf\u5929\u4e0d\u5b9a\u65f6\u66f4\u65b0\u6a21\u5f0f\uff0c\u8bf7\u70b9\u4e0a\u65b9\u4e0b\u8f7d\u56fe\u6807\u66f4\u65b0\u6a21\u5f0f\u554a"},
+{"id":5003,
 "type":"amy",
 "name":"\u767e\u5ea6\u76f4\u8fde",
 "model":"{
@@ -29,7 +34,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5003,
+{"id":5004,
 "type":"amy",
 "name":"\u767e\u5ea6\u76f4\u8fde2",
 "model":"{
@@ -56,7 +61,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5004,
+{"id":5005,
 "type":"amy",
 "name":"\u767e\u5ea6\u76f4\u8fde3",
 "model":"{
@@ -83,7 +88,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5005,
+{"id":5006,
 "type":"amy",
 "name":"\u767e\u5ea6\u76f4\u8fde-\u5907\u7528\u6a21\u5f0f-\u4e0d\u9650\u901f-\u6ca1\u7f51\u7528\u8fd9\u4e2a",
 "model":"{
@@ -110,7 +115,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5006,
+{"id":5007,
 "type":"amy",
 "name":"\u767e\u5ea6\u76f4\u8fde-\u53ef\u6e38\u620f\u3001\u770b\u89c6\u9891-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
 "model":"{
@@ -137,7 +142,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5007,
+{"id":5008,
 "type":"amy",
 "name":"\u817e\u8baf\u738b\u5361\u300140G\u5185",
 "model":"{
@@ -164,7 +169,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5008,
+{"id":5009,
 "type":"amy",
 "name":"\u817e\u8baf\u738b\u53612\u300140G\u5185",
 "model":"{
@@ -191,7 +196,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5009,
+{"id":5010,
 "type":"amy",
 "name":"\u5927\u738b\u5361-\u5907\u7528\u6a21\u5f0f-\u4e0d\u9650\u901f-\u6ca1\u7f51\u7528\u8fd9\u4e2a",
 "model":"{
@@ -218,7 +223,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5010,
+{"id":5011,
 "type":"amy",
 "name":"\u5927\u738b\u5361-\u53ef\u6e38\u620f\u3001\u770b\u89c6\u9891-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
 "model":"{
@@ -245,7 +250,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5011,
+{"id":5012,
 "type":"amy",
 "name":"\u9489\u94891 \u9002\u7528\uff1a\u963f\u91cc\u5b9d\u5361\u3001\u6c83\u6d3e0\u5143\u963f\u91cc\u6743\u76ca\u5305\u3001\u51b0\u9489\u9489\u5361\u7b49\u963f\u91cc\u7cfb",
 "model":"{
@@ -272,7 +277,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5012,
+{"id":5013,
 "type":"amy",
 "name":"\u9489\u94892",
 "model":"{
@@ -299,7 +304,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5013,
+{"id":5014,
 "type":"amy",
 "name":"\u9489\u94893",
 "model":"{
@@ -326,7 +331,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5014,
+{"id":5015,
 "type":"amy",
 "name":"\u9489\u9489-\u5907\u7528\u6a21\u5f0f-\u4e0d\u9650\u901f-\u6ca1\u7f51\u7528\u8fd9\u4e2a",
 "model":"{
@@ -353,7 +358,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5014,
+{"id":5016,
 "type":"amy",
 "name":"\u9489\u9489-\u53ef\u6e38\u620f\u3001\u770b\u89c6\u9891-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
 "model":"{
@@ -380,7 +385,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5015,
+{"id":5017,
 "type":"amy",
 "name":"\u5408\u4f5cA \u9002\u7528\uff1a\u6296\u97f3/\u5934\u6761\u5b9a\u5411\u5305 \u5218\u90a6\u5361\u3001\u5bfc\u5b66\u6613\u5b66\u5361\u3001\u7535\u4fe1\u5927\u9ed1\u725b\u3001\u5c0f\u6b6a\u5361\u7b49",
 "model":"{
@@ -407,7 +412,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5016,
+{"id":5018,
 "type":"amy",
 "name":"\u5408\u4f5cA-\u53ef\u6e38\u620f\u3001\u770b\u89c6\u9891-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
 "model":"{
@@ -434,7 +439,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5017,
+{"id":5019,
 "type":"amy",
 "name":"\u5408\u4f5cA-\u5907\u7528\u6a21\u5f0f-\u4e0d\u9650\u901f-\u6ca1\u7f51\u7528\u8fd9\u4e2a",
 "model":"{
@@ -462,7 +467,7 @@
   \"Routers\": []
 }"},
 {
-"id":5018,
+"id":5020,
 "type":"amy",
 "name":"\u7535\u4fe1\u505c\u673a",
 "model":"{
@@ -506,7 +511,7 @@
     }
   }]
 }"},
-{"id":5019,
+{"id":5021,
 "type":"amy",
 "name":"\u7535\u4fe1\u505c\u673a2",
 "model":"{
@@ -533,7 +538,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5020,
+{"id":5022,
 "type":"amy",
 "name":"\u505c\u673a-\u5907\u7528\u6a21\u5f0f-\u4e0d\u9650\u901f-\u6ca1\u7f51\u7528\u8fd9\u4e2a",
 "model":"{
@@ -560,7 +565,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5021,
+{"id":5023,
 "type":"amy",
 "name":"\u505c\u673a-\u53ef\u6e38\u620f\u3001\u770b\u89c6\u9891-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
 "model":"{
@@ -587,7 +592,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5022,
+{"id":5024,
 "type":"amy",
 "name":"\u5168\u56fd\u8054\u901a\u624b\u5385\u3001\u5e7f\u897f\u3001\u91cd\u5e86 \u53ef\u514d\uff0c\u5177\u4f53\u81ea\u6d4b",
 "model":"{
@@ -614,7 +619,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5023,
+{"id":5025,
 "type":"amy",
 "name":"\u5168\u56fd\u8054\u901a\u516c\u514d\uff0cWAP\u63a5\u5165\u70b9\u3001\u6ce8\u610f\u8981\u7528WAP\u63a5\u5165\u70b9 \u770b\u6e05\u695a\uff0c\u5177\u4f53\u81ea\u6d4b",
 "model":"{
@@ -641,7 +646,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5024,
+{"id":5026,
 "type":"amy",
 "name":"\u54d4\u54e9\u54d4\u54e9 \u9002\u7528\uff1a22\u5361\u300133\u5361\u3001B\u7ad9\u5b9a\u5411\u7b49",
 "model":"{
@@ -668,7 +673,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5025,
+{"id":5026,
 "type":"amy",
 "name":"\u54d4\u54e9\u54d4\u54e9-\u5907\u7528\u6a21\u5f0f-\u4e0d\u9650\u901f-\u6ca1\u7f51\u7528\u8fd9\u4e2a",
 "model":"{
@@ -695,7 +700,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5026,
+{"id":5027,
 "type":"amy",
 "name":"\u54d4\u54e9\u54d4\u54e9-\u53ef\u6e38\u620f\u3001\u770b\u89c6\u9891-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
 "model":"{
@@ -722,7 +727,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5027,
+{"id":5028,
 "type":"amy",
 "name":"\u5929\u7ffc\u4e91\u76d81 \u9002\u7528\uff1a\u7535\u4fe1\u5929\u7ffc\u4e91\u76d8\u5b9a\u5411\u6d41\u91cf\u5305",
 "model":"{
@@ -760,7 +765,7 @@
     }
   }]
 }"},
-{"id":5028,
+{"id":5029,
 "type":"amy",
 "name":"\u5929\u7ffc\u4e91\u76d82",
 "model":"{
@@ -787,7 +792,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5029,
+{"id":5030,
 "type":"amy",
 "name":"\u5929\u7ffc\u4e91\u76d8-\u5907\u7528\u6a21\u5f0f-\u4e0d\u9650\u901f-\u6ca1\u7f51\u7528\u8fd9\u4e2a",
 "model":"{
@@ -814,7 +819,7 @@
   }],
   \"Routers\": []
 }"},
-{"id":5030,
+{"id":5031,
 "type":"amy",
 "name":"\u5929\u7ffc\u4e91\u76d8-\u53ef\u6e38\u620f\u3001\u770b\u89c6\u9891-\u9650\u901f-\u4f4e\u5ef6\u8fdf",
 "model":"{
